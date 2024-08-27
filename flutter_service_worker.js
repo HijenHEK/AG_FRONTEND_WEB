19a5712b22ada9e0bcf5bb91a4a9af9970c8a3f1
mac <km@softifi.com>
2024-08-27 08:33:21 +0100

    session management

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "087f71430f6f8a56b80a09acac852be4",
+const RESOURCES = {"flutter_bootstrap.js": "1a657e78ee775b50f7001ee5dca5a1d3",
 "version.json": "a01cc84f3110634b5f3281ac02b43768",
 "index.html": "2ff614976ddbdd453797dbf24af4d286",
 "/": "2ff614976ddbdd453797dbf24af4d286",
-"main.dart.js": "d07d0ae0aa4e5009355d70bd2d4fc6f1",
+"main.dart.js": "fc0346b0b12b165f39e94f3b805e0389",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
